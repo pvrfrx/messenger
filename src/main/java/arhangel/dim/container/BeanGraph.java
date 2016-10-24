ff489afc8efd2494b5bc6cd609fcce6024c25571
pvrfrx@gmail.com <javarush1>
2016-10-24 17:17:45 +0500

    Task02*.Done

--- src/main/java/arhangel/dim/container/BeanGraph.java
+++ src/main/java/arhangel/dim/container/BeanGraph.java
@@ -1,10 +1,11 @@
 package arhangel.dim.container;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
 import java.util.Map;
+import java.util.Map.Entry;
 
 
 /**
@@ -16,19 +17,19 @@ public class BeanGraph {
 
     /**
      * Добавить вершину в граф
+     *
      * @param value - объект, привязанный к вершине
      */
     public BeanVertex addVertex(Bean value) {
         BeanVertex valueBeanVertex = new BeanVertex(value);
-        List<BeanVertex> valueBeanVertexList = null;
+        List<BeanVertex> valueBeanVertexList = new ArrayList<>();
 
         ArrayList<String> refList = haveProrertyRef(value);
         if (refList != null) {
             valueBeanVertexList = putRefInList(refList);
-            vertices.put(valueBeanVertex,valueBeanVertexList);
+            vertices.put(valueBeanVertex, valueBeanVertexList);
         } else {
-            valueBeanVertexList = new ArrayList<>();
-            vertices.put(valueBeanVertex,valueBeanVertexList);
+            vertices.put(valueBeanVertex, valueBeanVertexList);
         }
         verticesRefFromValue(valueBeanVertex); //ищем есть ли Классы зависимые от текущего Класса
         return valueBeanVertex;
@@ -51,8 +52,8 @@ public class BeanGraph {
     private void verticesRefFromValue(BeanVertex valueBeanVertex) {
         for (Entry<BeanVertex, List<BeanVertex>> entry :
                 vertices.entrySet()) {
-            for (Entry<String,Property> entry1:
-                     entry.getKey().getBean().getProperties().entrySet()) {
+            for (Entry<String, Property> entry1 :
+                    entry.getKey().getBean().getProperties().entrySet()) {
                 if (entry1.getValue().getValue().equals(valueBeanVertex.getBean().getName())) {
                     entry.getValue().add(valueBeanVertex);
                 }
@@ -62,7 +63,7 @@ public class BeanGraph {
 
     private ArrayList<String> haveProrertyRef(Bean value) {
         ArrayList<String> result = new ArrayList<>();
-        for (Entry<String,Property> entry :
+        for (Entry<String, Property> entry :
                 value.getProperties().entrySet()) {
             if (entry.getValue().getType() == ValueType.REF) {
                 result.add(entry.getValue().getValue());
@@ -77,11 +78,12 @@ public class BeanGraph {
 
     /**
      * Соединить вершины ребром
+     *
      * @param from из какой вершины
-     * @param to в какую вершину
+     * @param to   в какую вершину
      */
-    public void addEdge(BeanVertex from ,BeanVertex to) {
-        if (!isConnected(from,to)) {
+    public void addEdge(BeanVertex from, BeanVertex to) {
+        if (!isConnected(from, to)) {
             getLinked(from).add(to);
         }
     }
@@ -122,14 +124,85 @@ public class BeanGraph {
     }
 
     public static void main(String[] args) {
-
         BeanGraph beanGraph = new BeanGraph();
+
+
+
         List<Bean> beens = new BeanXmlReader().parseBeans("C:\\temp\\java\\mailru\\messenger\\config1.xml");
-        for (Bean been :
-                beens) {
-            beanGraph.addVertex(been);
-        }
+        beens.forEach(beanGraph::addVertex);
 
         System.out.println(beanGraph.toString());
+
+        CheckMap checkMap = new CheckMap(beanGraph.vertices);
+        checkMap.check();
+        System.out.println(checkMap);
+
+
+
+    }
+
+
+    private static class CheckMap {
+        Map<BeanVertex, List<BeanVertex>> vertices = new HashMap<>();
+        ArrayList<BeanVertex> whiteList = new ArrayList<>();
+        ArrayList<BeanVertex> greyList = new ArrayList<>();
+        ArrayList<BeanVertex> blackList = new ArrayList<>();
+
+        @Override
+        public String toString() {
+            String result = "\nThe order of initialization classes\n";
+            for (BeanVertex beanVertex :
+                    blackList) {
+                result += beanVertex + "\n";
+            }
+            return result;
+        }
+
+        public CheckMap(Map<BeanVertex, List<BeanVertex>> vertices) {
+            for (Entry<BeanVertex, List<BeanVertex>> entry : vertices.entrySet()) {
+                whiteList.add(entry.getKey());
+            }
+            this.vertices = new HashMap<>(vertices);
+        }
+
+        public void check() {
+            for (Entry<BeanVertex, List<BeanVertex>> entry : vertices.entrySet()) {
+                BeanVertex beanVertex = entry.getKey();
+                if (circleInMap(beanVertex)) {
+                    System.out.println("The circle found");
+                    return;
+                //    throw new IllegalArgumentException();
+                }
+            }
+            System.out.println("The circle did not found");
+
+
+        }
+
+        public boolean circleInMap(BeanVertex beanVertex) {
+            if (whiteList.contains(beanVertex)) {
+                if (greyList.contains(beanVertex)) {
+                    return true;
+                }
+                if (vertices.get(beanVertex).size() != 0) {
+                    greyList.add(beanVertex);
+                    for (BeanVertex refBean :
+                            vertices.get(beanVertex)) {
+                        if (!blackList.contains(refBean)) {
+                            if (circleInMap(refBean)) {
+                                return true;
+                            }
+                        }
+                    }
+                    greyList.remove(beanVertex);
+                    blackList.add(beanVertex);
+                    whiteList.remove(beanVertex);
+                } else {
+                    blackList.add(beanVertex);
+                    whiteList.remove(beanVertex);
+                }
+            }
+            return false;
+        }
     }
 }
